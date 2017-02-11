1ee7ac510f9b8616a349bbfd09b89aefeb238a08
patelrohanv <patelrohanv@gmail.com>
2017-02-11 16:15:19 -0500

    Fixes

--- TwitterBot/ConsoleApplication1/pybot.py
+++ TwitterBot/ConsoleApplication1/pybot.py
@@ -25,15 +25,18 @@ badwords = txtTwo
 ignoreWordsArray = ['youtube', 'twitch', 'instagram', 'facebook', 'twitter', 'snapchat', 'reddit', 'tumblr', 'myspace']
 #print(txtOne)
 #print(txtTwo)
-wordcount = [len(games)]
+#print(len(games))
+wordcount = [None] * len(games)
+#print(len(wordcount))
 i = -1
 flag = False
 for game in games:
     i = i+1
     count = 0
+    #print(i)
     wordcount[i] = count
     for word in badwords:
-        for tweet in tweepy.Cursor(api.search,q=[game,word]).items(100):
+        for tweet in tweepy.Cursor(api.search,q=[game,word]).items(10):
             for ignoreword in ignoreWordsArray:
                 if ignoreword in tweet.text:
                     flag = True 
@@ -43,7 +46,5 @@ for game in games:
                 wordcount[i] = count
                 #print('Tweet by: @' + tweet.user.screen_name)
                 #print(tweet.text)
-
 for i in range(0,len(games)):
-    print(games[i])
-    print(wordcount[i])
+    print(games[i] + ": " + wordcount[i])
